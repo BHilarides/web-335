e6ac2100c3c3c26da0e1918156384916f112131c
Ben Hilarides <ben.hilarides26@gmail.com>
2025-08-26 23:33:59 -0400

    final 2.3

---
+++ week-2/project/ben_hilarides_exercise2.3.335.py
@@ -0,0 +1,24 @@
+"""
+Author: Ben Hilarides
+Date: 08/26/2025
+Filename: ben_hilarides_exercise2.3.335.py
+Description: This file prints a quote from Roman emperor and philosopher Marcus Aurelius.
+"""
+
+# Defining the quote as a string variable
+quote = "You have power over your mind - not outside events. Realize this, and you will find strength."
+
+# Defining the author attribution
+author = "Marcus Aurelius"
+
+# Printing the quote with proper formatting
+print(f'"{quote}"')
+
+# Printing author attribution with a preceding dash
+print(f' - {author}')
+
+# Adding blank line for readability
+print()
+
+# Printing additional context about quote
+print("This quote is from 'Meditations', written by Marcus Aurelius in 161-180 AD.")
